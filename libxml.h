11db23577f8068ca966f729d6f180fae87e7dee4
Anton Pogonets <apogonets@readdle.com>
2017-08-23 17:37:02 +0300

    Initial

---
+++ libxml.h
@@ -0,0 +1,7 @@
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xmlstring.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+#include <libxml/SAX.h>
